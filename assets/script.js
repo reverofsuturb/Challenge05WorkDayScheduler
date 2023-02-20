93f059dafd2af017bfb91c887c0124262d0ec118
suturbreverof <sfriedman6711@gmail.com>
2023-02-20 10:57:56 -0700

    added realtime functionality

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,14 @@ var savebutton = $(".saveBtn");
 var divselect = $("div");
 // declared global variables
 function displaydate() { 
-today.text(dayjs().format('MMMM D, YYYY'))
+today.text(dayjs().format('MMMM D, YYYY hh:mm:ss A'))
 }
-displaydate();
 
-// wrapped this in a function per the assignment asked, this is simply putting the date above the planner itself using dayjs, and formatted it for clarity
+setInterval(displaydate, 100)
 
+// wrapped this in a function per the assignment asked, this is simply putting the date above the planner itself using dayjs, and formatted it for clarity, used set interval to add a nice ticking clock, used a little lower of a refresh rate as the load was a little glaring on page
+
+function hourcolor() {
 divselect.each(function () {
   if ($(this).attr("id") < currenthour) {
     $(this).removeClass("future", "present");
@@ -22,8 +24,11 @@ divselect.each(function () {
     $(this).addClass("future");
   }
 });
+}
 
-// this function will go through each div/timeblock and look at it's id and match it to the current hour, used the == selector as was having issues with the strict equality operator here so that it is comparing values instead, css class filters will be applied and removed accordingly
+setInterval(hourcolor, 100)
+
+// this function will go through each div/timeblock and look at it's id and match it to the current hour, used the == selector as was having issues with the strict equality operator here so that it is comparing values instead, css class filters will be applied and removed accordingly, I put it on set interval so that it will update as the day goes on, shortened the refresh rate as it was a little glaring on page load
 
 savebutton.on("click", function () {
   var scheduled = $(this).siblings("#recorded").val();
